6951045ef20b384141ae0604c629b1124ab218b8
Amanda Xavier <amandavxavier@gmail.com>
2023-05-28 20:05:47 -0300

    Fix: import function at projects.routes.js

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/schemaValidation.js";
 import {productDeliverySchema} from "../schemas/projectsSchemas.js";
-import { getProjectsByClasses, projectDelivery } from "../controllers/projects.controllers.js";
+import { getAllProjects, getProjectsByClasses, projectDelivery } from "../controllers/projects.controllers.js";
 
 const projectsRouter = Router();
 
