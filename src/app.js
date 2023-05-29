37b187fa17f612397e2bbe4e18a589175494dedc
Amanda Xavier <amandavxavier@gmail.com>
2023-05-28 23:44:57 -0300

    Feat: npm cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/index.routes.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router);
 dotenv.config();
 
